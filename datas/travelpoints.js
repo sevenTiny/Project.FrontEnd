0aad8e7df395373c8d4d0ca1f7586fd8fd36f7bb
seventiny <seventiny@seventinydeMacBook.local>
2020-02-05 19:22:31 +0800

    travel地图站点展示demo完成:zap:

---
+++ datas/travelpoints.js
@@ -0,0 +1,87 @@
+var travelpoints =
+    [
+        {
+            "id": 0,
+            "datetime": "20190305",
+            "title": "这是一个起点",
+            "line_color": "#18a45b",
+            "line_strokeOpacity": 0.8,
+            "line_strokeWeight": "3",
+            "points": [
+                {
+                    "id": 0,
+                    "point": [116, 30],
+                    "title": "temp111",
+                    "content": "11111",
+                    "img": "http://lbsyun.baidu.com/jsdemo/img/tianAnMen.jpg",
+                    "moreImgs": ""
+                },
+                {
+                    "id": 0,
+                    "point": [117, 35],
+                    "title": "temp222",
+                    "content": "22222",
+                    "img": "http://lbsyun.baidu.com/jsdemo/img/tianAnMen.jpg",
+                    "moreImgs": ""
+                },
+                {
+                    "id": 0,
+                    "point": [118, 40],
+                    "title": "temp333",
+                    "content": "33333",
+                    "img": "http://lbsyun.baidu.com/jsdemo/img/tianAnMen.jpg",
+                    "moreImgs": ""
+                },
+                {
+                    "id": 0,
+                    "point": [123, 35],
+                    "title": "temp222",
+                    "content": "22222",
+                    "img": "http://lbsyun.baidu.com/jsdemo/img/tianAnMen.jpg",
+                    "moreImgs": ""
+                },
+                {
+                    "id": 0,
+                    "point": [120, 40],
+                    "title": "temp333",
+                    "content": "33333",
+                    "img": "http://lbsyun.baidu.com/jsdemo/img/tianAnMen.jpg",
+                    "moreImgs": ""
+                }
+            ]
+        },
+        {
+            "id": 0,
+            "datetime": "20190305",
+            "title": "这是一个起点2",
+            "line_color": "#18a45e",
+            "line_strokeOpacity": 0.8,
+            "line_strokeWeight": "3",
+            "points": [
+                {
+                    "id": 0,
+                    "point": [130, 30],
+                    "title": "temp111",
+                    "content": "11111",
+                    "img": "http://lbsyun.baidu.com/jsdemo/img/tianAnMen.jpg",
+                    "moreImgs": ""
+                },
+                {
+                    "id": 0,
+                    "point": [131, 35],
+                    "title": "temp222",
+                    "content": "22222",
+                    "img": "http://lbsyun.baidu.com/jsdemo/img/tianAnMen.jpg",
+                    "moreImgs": ""
+                },
+                {
+                    "id": 0,
+                    "point": [132, 40],
+                    "title": "temp333",
+                    "content": "33333",
+                    "img": "http://lbsyun.baidu.com/jsdemo/img/tianAnMen.jpg",
+                    "moreImgs": ""
+                }
+            ]
+        }
+    ]
